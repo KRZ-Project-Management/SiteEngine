926393db7d8331227eba9d2ee34286f54d60b03b
Kharisma Antiko <39264103+kharismaantiko@users.noreply.github.com>
2022-10-23 18:57:10 +0700

    Add files via upload

--- krz-Pack3.js
+++ krz-Pack3.js
@@ -1,4 +1,3 @@
-$.urlParam = function(name){var results = new RegExp('[\?&]'+name+'=([^&#]*)').exec(window.location.href);if(results==null){return null} return decodeURI(results[1]) || 0;};
-function last_path(url){var rmvparam = url.split('?')[0]; var path = rmvparam.substring(rmvparam.lastIndexOf('/') + 1); return path;};
-function getUriWithParam(baseUrl, params){
-var Url = new URL(baseUrl); var urlParams = new URLSearchParams(Url.search); for (var key in params) { if (params[key] !== undefined) { urlParams.set(key, params[key]); } } Url.search = urlParams.toString(); return Url.toString();};
+function getParam(name){var results = new RegExp('[\?&]'+name+'=([^&#]*)').exec(window.location.href);if(results==null){return null} return decodeURI(results[1]) || 0;}
+function last_path(url){var rmvparam = url.split('?')[0]; var path = rmvparam.substring(rmvparam.lastIndexOf('/') + 1); return path;}
+function updateParam(key, value) {var uri=window.location.href,re = new RegExp("([?&])" + key + "=.*?(&|#|$)", "i");if( value === undefined || value === "") {if (uri.match(re)) {return uri.replace(re, '$1$2').replace(/[?&]$/, '').replaceAll(/([?&])&+/g, '$1').replace(/[?&]#/, '#');} else {return uri;}} else {if (uri.match(re)) {return uri.replace(re, '$1' + key + "=" + value + '$2');} else {var hash =  '';if( uri.indexOf('#') !== -1 ){hash = uri.replace(/.*#/, '#');uri = uri.replace(/#.*/, '');};var separator = uri.indexOf('?') !== -1 ? "&" : "?";return uri + separator + key + "=" + value + hash;}}}
